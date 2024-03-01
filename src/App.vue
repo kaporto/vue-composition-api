7a485e5f671a34c4d1af7dffa3f766aee3e389ec
karine <karine.porto@gmail.com>
2024-03-01 21:53:00 +0000

    Creating reactive reference with the ref function

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import { ref } from 'vue'
+
+export default{
+  setup(){
+    const message = ref("Hello")
+    const quantity = ref(1)
+
+    const increment = () => quantity.value++
+    const decrement = () => quantity.value--
+    return {
+      message,
+      quantity,
+      increment,
+      decrement
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h1>{{message}}</h1>
+  <h2>{{ quantity}}</h2>
+  <button class="button" @click="increment">+</button>
+  <button class="button" @click="decrement">-</button>
+
+</template>
+
+<style scoped>
+
+</style>
